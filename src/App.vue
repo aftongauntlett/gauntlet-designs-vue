7791593ca8d60a1971b53808f31ba55502b02480
Afton Gauntlett <afton.gauntlett@gmail.com>
2021-11-11 23:09:35 -0500

    adding components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="d-flex flex-column justify-content-between">
+    <Navbar />
+    <b-container
+        v-if="$route.name !== 'Home'"
+        fluid
+        class="flex-grow-1"
+        :class="$route.name === 'Home' ? '' : 'darkBackground'"
+    >
+      <router-view class="container"/>
+    </b-container>
+    <router-view v-else/>
+    <Footer/>
+
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    Footer,
+  },
+
+  // computed: {
+  //   imageUrl() {
+  //     return require("@/assets/images/desktop2.png");
+  //   },
+  // },
+};
+</script>
+
 
 <style lang="scss">
 #app {
@@ -17,16 +43,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footerFont">&copy; 2021 Copyright Gauntlet Designs, LLC</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.footerFont {
+  color: #ddd;
+  background-color: #282e34;
+  font-family: "Martel", serif;
+  padding: 20px;
+}
+</style>
